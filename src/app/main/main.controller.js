30cf51904e83fae567d3840c4fb8823c7c2e0a03
simonghales <simonghales@gmail.com>
2016-02-02 22:44:37 +1000

    intro

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -6,34 +6,29 @@
     .controller('MainController', MainController);
 
   /** @ngInject */
-  function MainController($timeout, webDevTec, toastr) {
+  function MainController($log) {
     var vm = this;
 
-    vm.awesomeThings = [];
-    vm.classAnimation = '';
-    vm.creationDate = 1454138821095;
-    vm.showToastr = showToastr;
+    vm.models = {
+      direct: ''
+    };
+
+    vm.states = {
+      tabs: {
+        active: 'upload'
+      }
+    };
+
+    vm.changeTab = changeTab;
 
     activate();
 
     function activate() {
-      getWebDevTec();
-      $timeout(function() {
-        vm.classAnimation = 'rubberBand';
-      }, 4000);
     }
 
-    function showToastr() {
-      toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
-      vm.classAnimation = '';
+    function changeTab(tab) {
+      vm.states.tabs.active = tab;
     }
 
-    function getWebDevTec() {
-      vm.awesomeThings = webDevTec.getTec();
-
-      angular.forEach(vm.awesomeThings, function(awesomeThing) {
-        awesomeThing.rank = Math.random();
-      });
-    }
   }
 })();
